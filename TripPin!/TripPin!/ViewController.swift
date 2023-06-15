780d179f6d5cdaeb44c2549c48f1f75a60d0804c
youngchive <2000mily@naver.com>
2023-06-15 23:13:09 +0900

    프로젝트 생성

---
+++ TripPin!/TripPin!/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TripPin!
+//
+//  Created by hansung on 2023/06/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
